15d7da21979ec08aa973daccb41f966261303528
MrSullivan <>
2021-04-20 21:37:04 +0000

    while true statemeent

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
 
+    sumOfNumbers = 0
+
+    while True:
+      number = int(input("Give a number: "))
+
+      if(number == 0):
+        break
+      
+      if(number != 0):
+        sumOfNumbers = sumOfNumbers + number
+      
+    print("Sum of the number: " + str(sumOfNumbers))
+
 if __name__ == '__main__':
     main()
